40efc71c461f2c1fd723941bbc7791c5b866163d
Anastasia <slyadneva.anastasia@gmail.com>
2020-04-23 14:47:17 -0500

    Updates

---
+++ precache-manifest.fa4a7b1584f18954915794dd6455d37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6243310e363baa2a4349eba147b18130",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "064e6d2e5ac38c288d3e",
+    "url": "/Todo-App/static/css/main.95c9b893.chunk.css"
+  },
+  {
+    "revision": "332e0bff2793bdda100b",
+    "url": "/Todo-App/static/js/2.fec0d95f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-App/static/js/2.fec0d95f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "064e6d2e5ac38c288d3e",
+    "url": "/Todo-App/static/js/main.1997dece.chunk.js"
+  },
+  {
+    "revision": "42f74c92bb559fe73303",
+    "url": "/Todo-App/static/js/runtime-main.255226a3.js"
+  }
+]);
